6f29f828a2aede0026f01a1f4fb2d94cb56a0cf9
mahdishokriyan <mahdi.shokriyan@gmail.com>
2024-07-14 19:51:42 -0400

    adding two more java class activity

---
+++ src/test/java/tek/intro/Activity01.java
@@ -0,0 +1,10 @@
+package tek.intro;
+
+public class Activity01 {
+    public static void main(String[] args) {
+        String name = "name";
+        int age = 1;
+        System.out.println(name);
+
+    }
+}
--- src/test/java/tek/intro/OpenGoogleOnly.java
+++ src/test/java/tek/intro/OpenGoogleOnly.java
@@ -5,8 +5,15 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class OpenGoogleOnly {
     public static void main(String[] args) {
 
-        var chrome = new ChromeDriver();
-        chrome.get("https://www.facebook.com/");
+        ChromeDriver chrome = new ChromeDriver();
+
+        chrome.manage().window().maximize();
+        chrome.get("https://www.google.com/");
+        String title = chrome.getTitle();
+        System.out.println(title);
+        chrome.quit();
+
+
 
 
     }
