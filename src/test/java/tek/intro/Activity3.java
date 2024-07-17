450381444a0609a24617d8c7b24f40f4336dc269
mahdishokriyan <mahdi.shokriyan@gmail.com>
2024-07-16 21:40:01 -0400

    some changes

--- src/test/java/tek/intro/Activity3.java
+++ src/test/java/tek/intro/Activity3.java
@@ -4,13 +4,26 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Activity3 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         ChromeDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("https://retail.tekschool-students.com/");
-        driver.findElement(By.id("searchInput")).sendKeys("TV");
-        driver.findElement(By.id("searchBtn")).click();
+        Thread.sleep(1000);
+//        driver.findElement(By.id("searchInput")).sendKeys("TV");
+//        driver.findElement(By.id("searchBtn")).click();
+        By videoGameLocator = By.xpath("//p[text()='Video Games']");
+        driver.findElement(videoGameLocator).click();
+        Thread.sleep(1000);
+        By ps5 = By.xpath("//p[text()='PlayStation 5 Console']");
+        driver.findElement(ps5).click();
+Thread.sleep(1000);
+        By addToCart = By.xpath("//span[text()='Add to Cart']");
+        String text = driver.findElement(addToCart).getText();
+        System.out.println(text);
+
+
+
 
 
     }
